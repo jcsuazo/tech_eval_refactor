63987ba52177041e5a92323efadd19a45d96bdc0
jcsuazo <jsuazo9@gmail.com>
2019-07-15 08:51:16 -0400

    added auth middleware to views

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,16 @@ Route::get('/test', function (User $user) {
     //
 });
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::resource('/users', 'UsersController');
-Route::resource('/movies', 'MovieController');
-Route::get('/browse', 'MovieController@browse');
-Route::view('/dev', 'pages.developer');
+Route::resource('/users', 'UsersController')->middleware('auth');
+Route::resource('/movies', 'MovieController')->middleware('auth');
+Route::get('/browse', 'MovieController@browse')->middleware('auth');
+Route::view('/dev', 'pages.developer')->middleware('auth');
 
 
 // select movies.*, count(movies.id) as favorite_count FROM movies JOIN movie_user ON movies.id = movie_user.movie_id GROUP BY movies.id ORDER BY `favorite_count` DESC
